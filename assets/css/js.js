24a706fd651a6be6cf9c044bffca55ff0c371ff1
adaazis <adaazis@gmail.com>
2026-06-01 12:07:48 +0200

    Update js.js

--- assets/css/js.js
+++ assets/css/js.js
@@ -38,3 +38,7 @@ dots[slideIndex-1].className += " active";
 // slides[slideIndex-1].style.display = "block";
 // setTimeout(showSlides, 2000); // Change image every 2 seconds
 // }
+
+$('.carousel').carousel({
+  interval: 2000
+})
