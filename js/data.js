fd3bbc43c83f096feeadd168617eb6fe92e9dbaa
adaazis <adaazis@gmail.com>
2026-06-02 12:42:37 +0200

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,6 +1,6 @@
 
 const ARTWORKS=[
-{id:'griffin',title:'Griffin',category:'Mythology',image:'assets/F1.jepg',description:'Replace with your description.'},
-{id:'duality',title:'Duality',category:'Human Form',image:'assets/F2.jepg',description:'Replace with your description.'},
-{id:'metamorphosis',title:'Metamorphosis',category:'Transformation',image:'assetsF3.jepg',description:'Replace with your description.'}
+{id:'griffin',title:'Griffin',category:'Mythology',image:'assets/F1.jpeg',description:'Replace with your description.'},
+{id:'duality',title:'Duality',category:'Human Form',image:'assets/F2.jpeg',description:'Replace with your description.'},
+{id:'metamorphosis',title:'Metamorphosis',category:'Transformation',image:'assetsF3.jpeg',description:'Replace with your description.'}
 ];
