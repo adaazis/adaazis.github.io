7bb0c0f4cbddde8119ed316074952489a43f33fb
adaazis <adaazis@gmail.com>
2026-06-02 13:14:24 +0200

    Add new artworks to the ARTWORKS array

--- js/data.js
+++ js/data.js
@@ -2,5 +2,8 @@
 const ARTWORKS=[
 {id:'griffin',title:'Griffin',category:'Mythology',image:'assets/F1.jpeg',description:'Replace with your description.'},
 {id:'duality',title:'Duality',category:'Human Form',image:'assets/F2.jpeg',description:'Replace with your description.'},
-{id:'metamorphosis',title:'Metamorphosis',category:'Transformation',image:'assets/F3.jpeg',description:'Replace with your description.'}
+{id:'metamorphosis',title:'Metamorphosis',category:'Transformation',image:'assets/F3.jpeg',description:'Replace with your description.'},
+{id:'couple',title:'Couple',category:'couple',image:'assets/F4.jpg',description:'Replace with your description.'},
+{id:'body',title:'Body',category:'Human Form',image:'assets/F5.jpg',description:'Replace with your description.'},
+{id:'change',title:'Change',category:'Transformation',image:'assets/F6.jpg',description:'Replace with your description.'}
 ];
